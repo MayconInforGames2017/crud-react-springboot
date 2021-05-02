944d90805c611372c0920c1cb36d214d34b12fad
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-05-02 14:49:07 -0300

    modificando app.js para a chamada /api/grupos e exibição da lista na IU, adicionando a configuracao de proxy para app/package.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,44 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    grupos: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/grupos');
+    const body = await response.json();
+    this.setState({ grupos: body, isLoading: false });
+  }
+
+  render() {
+    const { grupos, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Carregando...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>JUG LIST</h2>
+            { grupos.map(grupo => 
+              <div key={ grupo.id }>
+                {grupo.name}
+              </div>
+              ) }
+          </div>
+        </header>
+      </div>
+    )
+
+  }
+
 }
 
 export default App;
